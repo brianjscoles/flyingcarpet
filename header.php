544bea652c4381564591c0f058697097e1012fd7
Brian Scoles <brianscoles@gmail.com>
2014-10-23 17:51:05 -0700

    new PHP branch.  refactor header.html to header.php

--- header.php
+++ header.php
@@ -1,8 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
   <head>
-
-    <title>Flying Carpet Rug Restoration :: Tatjana Kopp</title>  
+    <title>Flying Carpet Rug Restoration :: Tatjana Kopp</title>
     <meta name="description" content="We have nearly 30 years of experience working on fine antique and contemporary weavings in Germany and in the United States. At our home-studio in Oakland, we restore, stabilize and redye mostly Middle Eastern handwoven rugs, but also work on Navajos, European, Chinese and Tibetan carpets.">
     <meta name="keywords" content="rug restoration, carpet restoration, oakland rug restoration, berkeley rug restoration, san francisco rug restoration">
     <meta charset="utf-8">
@@ -10,20 +9,21 @@
     <meta property="og:url" content="http://www.flyingcarpetrestoration.com/" />
     <meta property="og:description" content="We have nearly 30 years of experience working on fine antique and contemporary weavings in Germany and in the United States. At our home-studio in Oakland, we restore, stabilize and redye mostly Middle Eastern handwoven rugs, but also work on Navajos, European, Chinese and Tibetan carpets."/>
     <meta property="og:site_name" content="Flying Carpet Rug Restoration"/>
-    <meta property="og:image" content="images/sun-rug-final.jpg"/>
+    <meta property="og:image" content="http://www.flyingcarpetrestoration.com/images/sun-rug-final.jpg"/>
     <meta property="og:type" content="website" />
-    <link rel="shortcut icon" href="images/sun-rug-final.jpg">
-    <link href="http://fonts.googleapis.com/css?family=Berkshire+Swash" rel="stylesheet" type="text/css">
-    <link href="http://fonts.googleapis.com/css?family=Merienda+One" rel="stylesheet" type="text/css">
+    <link rel="shortcut icon" href="http://www.flyingcarpetrestoration.com/images/sun-rug-final.jpg">
+    <script type="text/javascript">if(top.location!=location){top.location.href=document.location.href;}</script>
     <link href="style.css" rel="stylesheet" type="text/css" />
+    
     <div class="header1">&nbsp;</div>
     <div class="banner" align="center">Flying Carpet Restoration Service</div>
-    <div class="header2" style="padding-top: 15px; padding-bottom: 20px;">&nbsp;</div>
-  </head>
-  <body>
-    <div class="navH">
-    	<a href="home.html"><div class="one">Home</div></a>
-        <a href="photos.html"><div class="two">Photos</div></a>
-        <a href="contact.html"><div class="three">Contact</div></a>
-        <a href="ourWork.html"><div class="four">Our Work</div></a>
+    <div class="newNav" align="center">
+      <a href="home.php" class="navbutton">Home</a>
+      <a href="photos.php" class="navbutton">Gallery</a>
+      <a href="contact.php" class="navbutton">Contact</a>
+      <a href="ourWork.php" class="navbutton">Our Work</a>
     </div>
+    <div class="header2">&nbsp;</div>
+  </head>
+
+  <body>
